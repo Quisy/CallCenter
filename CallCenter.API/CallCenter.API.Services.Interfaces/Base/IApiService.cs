b6ae001a1f1c6175a0183446ae322a588930fcc2
Quisy <lamprecht.mariusz@gmail.com>
2017-06-15 11:17:42 +0200

    Fix

--- CallCenter.API/CallCenter.API.Services.Interfaces/Base/IApiService.cs
+++ CallCenter.API/CallCenter.API.Services.Interfaces/Base/IApiService.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CallCenter.API.Services.Interfaces.Base
+﻿namespace CallCenter.API.Services.Interfaces.Base
 {
     public interface IApiService : IBaseService
     {
-        string GetBasicAuthorizationHeaderValue();
+        string GetBasicAuthorizationHeaderValue(string username, string password);
     }
 }
