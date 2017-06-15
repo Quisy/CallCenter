9e10f44c685c4a86639636501bf7e5a96c907603
Quisy <lamprecht.mariusz@gmail.com>
2017-06-15 11:31:15 +0200

    Added worker projects

--- CallCenter.API/CallCenter.API.Services/Base/ApiService.cs
+++ CallCenter.API/CallCenter.API.Services/Base/ApiService.cs
@@ -10,5 +10,6 @@ namespace CallCenter.API.Services.Base
         {
             return System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password));
         }
+
     }
 }
